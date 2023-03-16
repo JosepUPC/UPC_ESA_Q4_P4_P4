fb03d5d1162e00cde606ecfe5692e6a80ee14cbf
JosepUPC <github.jeb@gmail.com>
2023-03-16 10:14:35 +0100

    Master_AddP4_B

--- P4_B/src/main.cpp
+++ P4_B/src/main.cpp
@@ -1,56 +1,29 @@
-/*
-practica3.MD 17/3/2021
-2 / 4
-ESP32 Web Server - STA Mode
-modified on 25 MAy 2019
-by Mohammadreza Akbari @ Electropeak
-https://electropeak.com/learn
-*/
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
 
-#include <WiFi.h>
-#include <WebServer.h>
-// SSID & Password
+#include "BluetoothSerial.h"
 
-const char* ssid = "*****";
-const char* password = "*****";
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
-WebServer server(80);
+BluetoothSerial SerialBT;
 
 void setup() {
-
-Serial.begin(115200);
-Serial.println("Try Connecting to ");
-Serial.println(ssid);
-
-WiFi.begin(ssid, password);
-
-while (WiFi.status() != WL_CONNECTED) {
-
-delay(1000);
-Serial.print(".");
-
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
 }
-Serial.println("");
-Serial.println("WiFi connected successfully");
-Serial.print("Got IP: ");
-Serial.println(WiFi.localIP());
-server.on("/", handle_root);
-server.begin();
-Serial.println("HTTP server started");
-delay(100);
 
-}
 void loop() {
-server.handleClient();
-}
-
-String HTML = "<!DOCTYPE html>\
-<html>\
-<body>\
-<h1>My Primera Pagina con ESP32 - Station Mode &#128522;</h1>\
-</body>\
-</html>";
-
-void handle_root() {
-server.send(200, "text/html", HTML);
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
 }
